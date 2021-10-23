6e7ad3a2e74e545a4e1d167e50cb0f1dbfa76c1a
ticTpoc <william_190_@hotmail.com>
2021-10-22 22:49:45 -0300

    dungeon 2.0
    de alta qualidade.

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -2,3 +2,10 @@
 function Alertar(valor){
     alert(valor);
 }
+
+function randomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
